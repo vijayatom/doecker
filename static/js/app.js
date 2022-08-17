62479a24668bef561c72d1d763c868278e4e3226
Ramesh Patel <rameshpatel.d1516@gmail.com>
2022-08-17 12:47:51 +0530

    modified:   app/__pycache__/settings.cpython-39.pyc 	modified:   app/__pycache__/urls.cpython-39.pyc 	modified:   app/settings.py 	modified:   app/urls.py 	renamed:    main/templates/index.html -> main/templates/index22.html 	modified:   main/templates/main.html 	new file:   static/assests/gi tag.png 	new file:   static/assests/india emb.png 	new file:   static/assests/laddu.jpeg 	new file:   static/css/main.css 	new file:   static/js/app.js

---
+++ static/js/app.js
@@ -0,0 +1,102 @@
+// COUNTER SECTION ######
+const counters = document.querySelectorAll(".counter-value");
+const speed = 1000;
+
+counters.forEach((counter) => {
+  const animate = () => {
+    const value = +counter.getAttribute("akhi");
+    const data = +counter.innerText;
+
+    const time = value / speed;
+    if (data < value) {
+      counter.innerText = Math.ceil(data + time);
+      setTimeout(animate, 1);
+    } else {
+      counter.innerText = value;
+    }
+  };
+
+  animate();
+});
+
+window.addEventListener("resize", function () {
+  addRequiredClass();
+});
+
+function addRequiredClass() {
+  if (window.innerWidth < 860) {
+    document.body.classList.add("mobile");
+  } else {
+    document.body.classList.remove("mobile");
+  }
+}
+
+window.onload = addRequiredClass;
+
+let hamburger = document.querySelector(".hamburger");
+let mobileNav = document.querySelector(".nav-list");
+
+let bars = document.querySelectorAll(".hamburger span");
+
+let isActive = false;
+
+hamburger.addEventListener("click", function () {
+  mobileNav.classList.toggle("open");
+  if (!isActive) {
+    bars[0].style.transform = "rotate(45deg)";
+    bars[1].style.opacity = "0";
+    bars[2].style.transform = "rotate(-45deg)";
+    isActive = true;
+  } else {
+    bars[0].style.transform = "rotate(0deg)";
+    bars[1].style.opacity = "1";
+    bars[2].style.transform = "rotate(0deg)";
+    isActive = false;
+  }
+});
+
+// COROUSEAL ######
+let mainPosts = document.querySelectorAll(".main-post");
+let posts = document.querySelectorAll(".post");
+
+let i = 0;
+let postIndex = 0;
+let currentPost = posts[postIndex];
+let currentMainPost = mainPosts[postIndex];
+
+let progressInterval = setInterval(progress, 100); // 180
+
+function progress() {
+  if (i === 100) {
+    i = -5;
+    // reset progress bar
+    currentPost.querySelector(".progress-bar__fill").style.width = 0;
+    document.querySelector(
+      ".progress-bar--primary .progress-bar__fill"
+    ).style.width = 0;
+    currentPost.classList.remove("post--active");
+
+    postIndex++;
+
+    currentMainPost.classList.add("main-post--not-active");
+    currentMainPost.classList.remove("main-post--active");
+
+    // reset postIndex to loop over the slides again
+    if (postIndex === posts.length) {
+      postIndex = 0;
+    }
+
+    currentPost = posts[postIndex];
+    currentMainPost = mainPosts[postIndex];
+  } else {
+    i++;
+    currentPost.querySelector(".progress-bar__fill").style.width = `${i}%`;
+    document.querySelector(
+      ".progress-bar--primary .progress-bar__fill"
+    ).style.width = `${i}%`;
+    currentPost.classList.add("post--active");
+
+    currentMainPost.classList.add("main-post--active");
+    currentMainPost.classList.remove("main-post--not-active");
+  }
+}
