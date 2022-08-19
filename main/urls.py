73c37ad18a72ca0ca42c97accf912431ff88054f
Ramesh Patel <rameshpatel.d1516@gmail.com>
2022-08-19 11:54:11 +0530

    modified:   main/__pycache__/models.cpython-39.pyc 	modified:   main/__pycache__/urls.cpython-39.pyc 	modified:   main/__pycache__/views.cpython-39.pyc 	new file:   main/templates/about-content.html 	modified:   main/templates/about.html 	modified:   main/templates/contact.html 	modified:   main/templates/faq.html 	modified:   main/templates/login.html 	modified:   main/templates/main.html 	modified:   main/templates/newsfeed-main.html 	modified:   main/templates/secondary-navbar.html 	modified:   main/urls.py 	new file:   static/assests/Untitled design.png 	new file:   static/assests/abt-banner.jpeg 	deleted:    static/css/about.css 	new file:   static/css/contact.css 	deleted:    static/css/faq.css 	new file:   static/css/login.css 	modified:   static/css/main.css 	new file:   static/js/login.js

--- main/urls.py
+++ main/urls.py
@@ -3,14 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('about', views.about),
+    path('main', views.home),
+    path('about/', views.about),
     path('registration', views.registration),
     path('renewal', views.renewal),
     path('contact', views.contact),
     path("form1/<form_type>", views.create_form1, name="form1"),
     path('faq', views.faq),
-    path('login', views.login),
-    
-
+    path('login', views.login),  
 ]
